35ef2dea12eeafcef76c4f65a072f972a2009047
Relsi Maron <relsi@codeup.com.br>
2020-04-15 14:54:37 -0300

    Rewrite the Article CRUD using class based views and DRF Response method and APIView class

--- newsapi/news/api/urls.py
+++ newsapi/news/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from news.api.views import article_api_view, article_detail_api_view
+# from news.api.views import article_api_view, article_detail_api_view
+from news.api.views import ArticleAPIView, ArticleDetailView
 
 urlpatterns = [
-    path("articles/", article_api_view, name="article-list"),
-    path("articles/<int:pk>/", article_detail_api_view, name="article-detail")
+    path("articles/", ArticleAPIView.as_view(), name="article-list"),
+    path("articles/<int:pk>/", ArticleDetailView.as_view(), name="article-detail")
+    # path("articles/", article_api_view, name="article-list"),
+    # path("articles/<int:pk>/", article_detail_api_view, name="article-detail")
 ]
