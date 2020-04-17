22a968d330d090701538ec30a2e04ae2b93d75b3
Relsi Maron <relsi@codeup.com.br>
2020-04-17 18:25:55 -0300

    Use DRF router to create the endpoints routes for the view class

--- profilesapi/profiles/api/urls.py
+++ profilesapi/profiles/api/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from profiles.api.views import ProfileViewSet
 
-profile_list = ProfileViewSet.as_view({"get": "list"})
-profile_detail = ProfileViewSet.as_view({"get": "retrieve"})
+# router views created using viewset
+# profile_list = ProfileViewSet.as_view({"get": "list"})
+# profile_detail = ProfileViewSet.as_view({"get": "retrieve"})
+
+#user router to create the routes
+router = DefaultRouter()
+router.register(r"profiles", ProfileViewSet)
 
 urlpatterns = [
-    path("profiles/", profile_list, name="profile-list"),
-    path("profiles/<int:pk>/", profile_detail, name="profile-detail")
+    path("", include(router.urls))
+    # path("profiles/", profile_list, name="profile-list"),
+    # path("profiles/<int:pk>/", profile_detail, name="profile-detail")
 ]
