378ec1554d784ea62d88ca8a66a2250021d984c3
Relsi Maron <relsi@codeup.com.br>
2020-04-17 18:21:28 -0300

    Use DRF ViewSet to create the endpoints using the same view class

--- profilesapi/profiles/api/urls.py
+++ profilesapi/profiles/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from profiles.api.views import ProfileList
+from profiles.api.views import ProfileViewSet
+
+profile_list = ProfileViewSet.as_view({"get": "list"})
+profile_detail = ProfileViewSet.as_view({"get": "retrieve"})
 
 urlpatterns = [
-    path("profiles/", ProfileList.as_view())
+    path("profiles/", profile_list, name="profile-list"),
+    path("profiles/<int:pk>/", profile_detail, name="profile-detail")
 ]
--- profilesapi/profiles/api/views.py
+++ profilesapi/profiles/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from profiles.models import Profile
 from profiles.api.serializers import ProfileSerializer
 
-class ProfileList(generics.ListAPIView):
+class ProfileViewSet(ReadOnlyModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
