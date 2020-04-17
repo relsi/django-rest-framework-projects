359c4057a6a8a2b9faeabc13eb8e8781cd291702
Relsi Maron <relsi@codeup.com.br>
2020-04-16 22:17:31 -0300

    Create the pagination Class

--- ebooksapi/ebooks/api/views.py
+++ ebooksapi/ebooks/api/views.py
@@ -3,16 +3,18 @@ from rest_framework import permissions
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 
+from ebooks.api.pagination import SmallSetPagination
 from ebooks.models import Ebook, Review
 from ebooks.api.serializers import EbookSerializer, ReviewSerializer
 from ebooks.api.permissions import IsAdminUserOrReadOnly, IsReviewAuthorOrReadOnly
 
 #concret generic views
 class EbookListCreateView(generics.ListCreateAPIView):
-    queryset = Ebook.objects.all()
+    queryset = Ebook.objects.all().order_by("-id")
     serializer_class = EbookSerializer
     permission_classes = [IsAdminUserOrReadOnly]
-
+    pagination_class = SmallSetPagination
+    
 class EbookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
