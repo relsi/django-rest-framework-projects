bbbf3adb89c4531e3e1b20f3d31ce39a56927052
Relsi Maron <relsi@codeup.com.br>
2020-04-17 18:32:10 -0300

    change the view class to generic viewset an use mixins to get update action

--- profilesapi/profiles/api/views.py
+++ profilesapi/profiles/api/views.py
@@ -1,10 +1,15 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ReadOnlyModelViewSet
+# from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework import viewsets
+from rest_framework import mixins
 from profiles.models import Profile
 from profiles.api.serializers import ProfileSerializer
 
-class ProfileViewSet(ReadOnlyModelViewSet):
+class ProfileViewSet(mixins.UpdateModelMixin,
+                     mixins.ListModelMixin,
+                     mixins.RetrieveModelMixin,
+                     viewsets.GenericViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
